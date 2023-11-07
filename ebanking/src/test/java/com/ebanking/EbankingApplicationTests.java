452b18f092ff3b508dbb49b35eb9b48445ce937b
AlexKaruiru <alexndegwa49@gmail.com>
2023-11-07 11:16:52 +0300

    dependencies, project setup

--- ebanking/src/test/java/com/ebanking/EbankingApplicationTests.java
+++ ebanking/src/test/java/com/ebanking/EbankingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ebanking;
+/*package com.ebanking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EbankingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+@Test
+void contextLoads() {
 }
+
+}*/
